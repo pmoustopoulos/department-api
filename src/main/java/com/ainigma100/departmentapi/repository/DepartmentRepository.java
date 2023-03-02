70d112815c33cf89a6d7ad707b94c04d1b75e96c
Petros Moustopoulos <pet.moustopoulos@gmail.com>
2023-03-02 11:06:46 +0200

    use text block
    Modify the custom query in the repository so that text block is used instead of string concatenation.

--- src/main/java/com/ainigma100/departmentapi/repository/DepartmentRepository.java
+++ src/main/java/com/ainigma100/departmentapi/repository/DepartmentRepository.java
@@ -13,10 +13,12 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Department findByDepartmentCode(String departmentCode);
 
 
-    @Query(value = "select dep from Department dep " +
-            "where ( :#{#criteria.departmentCode} IS NULL OR dep.departmentCode LIKE :#{#criteria.departmentCode}% ) " +
-            "and ( :#{#criteria.departmentName} IS NULL OR dep.departmentName LIKE :#{#criteria.departmentName}% ) " +
-            "and ( :#{#criteria.departmentDescription} IS NULL OR dep.departmentDescription LIKE %:#{#criteria.departmentDescription}% ) ")
+    @Query(value = """
+            select dep from Department dep
+            where ( :#{#criteria.departmentCode} IS NULL OR dep.departmentCode LIKE :#{#criteria.departmentCode}% )
+            and ( :#{#criteria.departmentName} IS NULL OR dep.departmentName LIKE :#{#criteria.departmentName}% )
+            and ( :#{#criteria.departmentDescription} IS NULL OR dep.departmentDescription LIKE %:#{#criteria.departmentDescription}% )
+            """)
     Page<Department> getAllDepartmentsUsingPagination(
             @Param("criteria") DepartmentSearchCriteriaDTO departmentSearchCriteriaDTO,
             Pageable pageable);
