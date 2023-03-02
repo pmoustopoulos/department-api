685f64dfdbb77b9d574ac0d52d7cc9b65147adb8
Petros Moustopoulos <pet.moustopoulos@gmail.com>
2023-03-02 11:06:46 +0200

    use text block
    Modify the custom query in the repository so that text block is used instead of string concatenation.

--- src/main/java/com/ainigma100/departmentapi/repository/EmployeeRepository.java
+++ src/main/java/com/ainigma100/departmentapi/repository/EmployeeRepository.java
@@ -14,10 +14,12 @@ public interface EmployeeRepository extends JpaRepository<Employee, String> {
 
     List<Employee> findByDepartmentId(Long departmentId);
 
-    @Query(value = "select emp from Employee emp " +
-            "where ( :#{#criteria.firstName} IS NULL OR emp.firstName LIKE :#{#criteria.firstName}% ) " +
-            "and ( :#{#criteria.lastName} IS NULL OR emp.lastName LIKE :#{#criteria.lastName}% ) " +
-            "and ( :#{#criteria.email} IS NULL OR emp.email LIKE %:#{#criteria.email}% ) ")
+    @Query(value = """
+            select emp from Employee emp
+            where ( :#{#criteria.firstName} IS NULL OR emp.firstName LIKE :#{#criteria.firstName}% )
+            and ( :#{#criteria.lastName} IS NULL OR emp.lastName LIKE :#{#criteria.lastName}% )
+            and ( :#{#criteria.email} IS NULL OR emp.email LIKE %:#{#criteria.email}% )
+            """)
     Page<Employee> getAllEmployeesUsingPagination(
             @Param("criteria") EmployeeSearchCriteriaDTO employeeSearchCriteriaDTO,
             Pageable pageable);
