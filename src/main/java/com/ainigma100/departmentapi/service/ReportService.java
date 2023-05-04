2fcf832cf5cb42b23ac7bedc3e833742d432fdc1
Petros Moustopoulos <pet.moustopoulos@gmail.com>
2023-05-04 20:51:25 +0300

    Generate multi-sheet excel report using sub reports

--- src/main/java/com/ainigma100/departmentapi/service/ReportService.java
+++ src/main/java/com/ainigma100/departmentapi/service/ReportService.java
@@ -14,4 +14,6 @@ public interface ReportService {
     FileDTO generatePdfFullReport() throws JRException;
 
     FileDTO generateAndZipReports() throws JRException, IOException;
+
+    FileDTO generateMultiSheetExcelReport() throws JRException;
 }
