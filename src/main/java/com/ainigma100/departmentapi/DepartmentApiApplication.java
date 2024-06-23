00299f2ce9f39c4c37f2ebc43b238a9a15bd81b5
Petros Moustopoulos <pet.moustopoulos@gmail.com>
2024-06-23 13:42:25 +0300

    Refactor server details logging and OpenAPI documentation generation
    Enhancements were made to both logging server details on application startup and generating OpenAPI documentation. For logging server details, we now specify a component for logging server details upon application startup, listening for ApplicationReadyEvent. Changes in OpenAPI documentation include outputting the configuration and generation to a separate configuration class and dynamically updating information such as title and version.

--- src/main/java/com/ainigma100/departmentapi/DepartmentApiApplication.java
+++ src/main/java/com/ainigma100/departmentapi/DepartmentApiApplication.java
@@ -1,27 +1,16 @@
 package com.ainigma100.departmentapi;
 
-import com.ainigma100.departmentapi.filter.ServerDetails;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@OpenAPIDefinition(info =
-@Info(title = "Department API", version = "${springdoc.version}", description = "Documentation ${spring.application.name} v1.0")
-)
 @SpringBootApplication
 public class DepartmentApiApplication {
 
     public static void main(String[] args) {
-
-        SpringApplication app = new SpringApplication(DepartmentApiApplication.class);
-        Environment environment = app.run(args).getEnvironment();
-        ServerDetails serverDetails = new ServerDetails(environment);
-        serverDetails.logServerDetails();
-
+        SpringApplication.run(DepartmentApiApplication.class, args);
     }
+
 
 }
