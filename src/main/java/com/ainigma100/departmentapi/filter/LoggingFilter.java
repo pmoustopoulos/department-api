cd0f18da3dc49f8c264852a05432e692c9105242
Petros Moustopoulos <pet.moustopoulos@gmail.com>
2024-10-06 00:04:57 +0300

    Update LoggingFilter to log client IP and change YAML placeholders
    Enhanced the LoggingFilter to include client IP addresses in the request logs. Updated application.yaml to replace occurrences of `@project.artifactId@` with `@project.name@`, aligning configurations with the new project naming conventions.

--- src/main/java/com/ainigma100/departmentapi/filter/LoggingFilter.java
+++ src/main/java/com/ainigma100/departmentapi/filter/LoggingFilter.java
@@ -8,6 +8,22 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * LoggingFilter is a servlet filter that logs HTTP requests and responses.
+ *
+ * <p>This filter logs the URL and method of incoming requests and the status
+ * of outgoing responses. It excludes certain paths from logging, such as those
+ * related to Actuator, Swagger, API documentation, favicon, and UI resources.</p>
+ *
+ */
 @Component
 @Slf4j
 public class LoggingFilter implements Filter {
@@ -20,11 +36,10 @@ public class LoggingFilter implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
+        String clientIP = this.getClientIP(httpServletRequest);
 
         if ( this.shouldLogRequest(httpServletRequest) ) {
-
-            log.info("Request {}, method: {}", httpServletRequest.getRequestURL().toString(), httpServletRequest.getMethod());
-
+            log.info("Client IP: {}, Request URL: {}, Method: {}", clientIP, httpServletRequest.getRequestURL(), httpServletRequest.getMethod());
         }
 
         // pre methods call stamps
@@ -32,7 +47,7 @@ public class LoggingFilter implements Filter {
 
         // post method calls stamps
         if ( this.shouldLogRequest(httpServletRequest) ) {
-            log.info("Response status {} ", httpServletResponse.getStatus());
+            log.info("Response status: {}", httpServletResponse.getStatus());
         }
 
     }
@@ -42,6 +57,25 @@ public class LoggingFilter implements Filter {
         // (?i) enables case-insensitive matching, \b matched as whole words
         // reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions
         return !request.getServletPath().matches("(?i).*\\b(actuator|swagger|api-docs|favicon|ui)\\b.*");
+    }
+
+    private String getClientIP(HttpServletRequest request) {
+
+        String clientIP = request.getHeader("Client-IP");
+
+        if (clientIP == null || clientIP.isEmpty() || "unknown".equalsIgnoreCase(clientIP)) {
+            clientIP = request.getHeader("X-Forwarded-For");
+        }
+
+        if (clientIP == null || clientIP.isEmpty() || "unknown".equalsIgnoreCase(clientIP)) {
+            clientIP = request.getHeader("X-Real-IP");
+        }
+
+        if (clientIP == null || clientIP.isEmpty() || "unknown".equalsIgnoreCase(clientIP)) {
+            clientIP = request.getRemoteAddr();
+        }
+
+        return clientIP != null ? clientIP : "Unknown";
     }
 
 }
